579ada1ea0ea852c555867e8f5ad7c77e1043766
roworu <mamoruessu@gmail.com>
2026-02-04 16:56:41 +0200

    + add some more test packages to base

--- build_files/1_install_base_packages.sh
+++ build_files/1_install_base_packages.sh
@@ -2,4 +2,13 @@
 
 set -ouex pipefail
 
-dnf5 install -y zsh konsole dolphin
+# cli tools
+dnf5 install -y zsh htop
+
+
+# steam
+dnf5 install -y steam
+
+# virt-manager
+dnf5 install -y @virtualization
+systemctl enable libvirtd
