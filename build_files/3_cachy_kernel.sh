a95610616ca07f7148af9136154a3754df8975b5
roworu <mamoruessu@gmail.com>
2026-02-06 16:22:18 +0200

    + use bazzite kernel installation method

--- build_files/3_cachy_kernel.sh
+++ build_files/3_cachy_kernel.sh
@@ -2,21 +2,41 @@
 
 set -ouex pipefail
 
+# sources:
+# 1) installation method:
+# https://github.com/ublue-os/bazzite/blob/main/build_files/install-kernel
+# 2) kernel itself:
 # https://github.com/CachyOS/copr-linux-cachyos
 
-# 1) install cachy kernel
 dnf5 -y copr enable bieszczaders/kernel-cachyos
-dnf5 -y install kmod
-dnf5 -y install \
-  kernel-cachyos \
-  kernel-cachyos-core \
-  kernel-cachyos-modules \
-  kernel-cachyos-modules-extra \
-  kernel-cachyos-devel-matched
-dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-dnf5 -y copr disable bieszczaders/kernel-cachyos
 
-# 2) setup selinux
-if command -v setsebool >/dev/null 2>&1 && command -v selinuxenabled >/dev/null 2>&1 && selinuxenabled; then
-  setsebool -P domain_kernel_load_modules on
-fi
+echo "::group::Executing install-kernel"
+trap 'echo "::endgroup::"' EXIT
+
+# create a shims to bypass kernel install triggering dracut/rpm-ostree
+# seems to be minimal impact, but allows progress on build
+pushd /usr/lib/kernel/install.d
+mv 05-rpmostree.install 05-rpmostree.install.bak
+mv 50-dracut.install 50-dracut.install.bak
+printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
+printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
+chmod +x  05-rpmostree.install 50-dracut.install
+popd
+
+dnf5 -y remove --no-autoremove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-tools kernel-tools-libs
+
+pkgs=(
+    kernel-cachyos
+    kernel-cachyos-devel-matched
+)
+
+dnf5 -y install $pkgs
+
+dnf5 versionlock add $pkgs
+
+pushd /usr/lib/kernel/install.d
+mv -f 05-rpmostree.install.bak 05-rpmostree.install
+mv -f 50-dracut.install.bak 50-dracut.install
+popd
+
+dnf5 -y copr disable bieszczaders/kernel-cachyos
