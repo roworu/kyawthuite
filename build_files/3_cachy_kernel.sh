d7324a00ae5aeffcca12d918fb67bc86ba25ccd7
roworu <mamoruessu@gmail.com>
2026-02-06 15:26:56 +0200

    + try to fix kernel install again

--- build_files/3_cachy_kernel.sh
+++ build_files/3_cachy_kernel.sh
@@ -6,8 +6,17 @@ set -ouex pipefail
 
 # 1) install cachy kernel
 dnf5 -y copr enable bieszczaders/kernel-cachyos
-dnf5 -y --setopt=tsflags=noscripts install kernel-cachyos kernel-cachyos-devel-matched
+dnf5 -y install kmod
+dnf5 -y install \
+  kernel-cachyos \
+  kernel-cachyos-core \
+  kernel-cachyos-modules \
+  kernel-cachyos-modules-extra \
+  kernel-cachyos-devel-matched
+dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 dnf5 -y copr disable bieszczaders/kernel-cachyos
 
 # 2) setup selinux
-setsebool -P domain_kernel_load_modules on
+if command -v setsebool >/dev/null 2>&1 && command -v selinuxenabled >/dev/null 2>&1 && selinuxenabled; then
+  setsebool -P domain_kernel_load_modules on
+fi
