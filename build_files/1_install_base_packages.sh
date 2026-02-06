2832c2b9c432126dbeee076a82f629900c93f284
roworu <mamoruessu@gmail.com>
2026-02-06 15:09:07 +0200

    + do not install weak deps for steam

--- build_files/1_install_base_packages.sh
+++ build_files/1_install_base_packages.sh
@@ -6,7 +6,7 @@ set -ouex pipefail
 dnf5 install -y zsh htop fastfetch plasma-discover-rpm-ostree
 
 # steam
-dnf5 install -y steam
+dnf5 install -y --setopt=install_weak_deps=False steam
 
 # virt-manager
 dnf5 install -y @virtualization
