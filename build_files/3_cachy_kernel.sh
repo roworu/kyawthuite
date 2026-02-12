cfdfae5998a6a91a6295d1ae53bbdb51dce61b53
roworu <mamoruessu@gmail.com>
2026-02-12 10:03:13 +0200

    cleanup kernel installation

--- build_files/3_cachy_kernel.sh
+++ build_files/3_cachy_kernel.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# 1) Enable Cachy COPR
+dnf -y remove \
+    kernel \
+    kernel-* &&
+    rm -r -f /usr/lib/modules/*
+
 dnf -y copr enable bieszczaders/kernel-cachyos-lto
 
-# 2) Install Cachy kernel alongside existing one
 dnf -y install --setopt=install_weak_deps=False kernel-cachyos-lto
 
-# 3) Disable COPR
 dnf -y copr disable bieszczaders/kernel-cachyos-lto
 
-# 4) Clean caches to keep image small
 dnf clean all
 rm -rf /var/cache/dnf
-
