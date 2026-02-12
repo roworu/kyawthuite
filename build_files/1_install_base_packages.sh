c16eadc69e06388e6c95a0c6dd3c717ae6bfa891
roworu <mamoruessu@gmail.com>
2026-02-12 10:03:02 +0200

    do not install steam via (prefer dnf later)

--- build_files/1_install_base_packages.sh
+++ build_files/1_install_base_packages.sh
@@ -5,8 +5,5 @@ set -ouex pipefail
 # cli tools
 dnf5 install -y zsh htop fastfetch plasma-discover-rpm-ostree
 
-# steam
-dnf5 install -y --setopt=install_weak_deps=False steam
-
 # virt-manager
 dnf5 install -y @virtualization; systemctl enable libvirtd
