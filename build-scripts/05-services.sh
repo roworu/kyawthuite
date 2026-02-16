1c9f7e3ffa234ff2abe334523360ea5cb1d85fe8
roworu <mamoruessu@gmail.com>
2026-02-16 23:54:04 +0200

    reenable services

--- build-scripts/05-services.sh
+++ build-scripts/05-services.sh
@@ -28,16 +28,16 @@ system_services=(
 )
 
 user_services=(
-#  dms.service
+  dms.service
   podman.socket
-#  dms-watch.path
-#  dsearch.service
-#  de-setup.service
-#  foot-server.service
-#  flathub-setup.service
-#  gnome-keyring-daemon.socket
-#  gnome-keyring-daemon.service
-#  dms-greeter-sync-trigger.service
+  dms-watch.path
+  dsearch.service
+  de-setup.service
+  foot-server.service
+  flathub-setup.service
+  gnome-keyring-daemon.socket
+  gnome-keyring-daemon.service
+  dms-greeter-sync-trigger.service
 )
 
 mask_services=(
