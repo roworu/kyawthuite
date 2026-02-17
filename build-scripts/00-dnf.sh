4e3eb10658bc40084c4d28f16683590ce7deee03
roworu <mamoruessu@gmail.com>
2026-02-17 12:44:14 +0200

    cleanup dnf setup

--- build-scripts/00-dnf.sh
+++ build-scripts/00-dnf.sh
@@ -1,50 +1,28 @@
-#!/bin/bash
-
-echo "::group:: ===$(basename "$0")==="
+#!/usr/bin/env bash
 
 set -ouex pipefail
 
-shopt -s nullglob
-
-coprs=(
-  bieszczaders/kernel-cachyos-lto
-  bieszczaders/kernel-cachyos-addons
-
-  ublue-os/packages
-
-#  yalter/niri
-#  ulysg/xwayland-satellite
-#  avengemedia/danklinux
-#  avengemedia/dms
-
-  che/nerd-fonts
-)
-
-mkdir -p /var/roothome
+# mkdir -p /var/roothome
 dnf5 -y install dnf5-plugins
 echo -n "max_parallel_downloads=10" >>/etc/dnf/dnf.conf
-RELEASE=$(rpm -E %fedora)
+# RELEASE=$(rpm -E %fedora)
 
+# rpmfusion repos
 dnf5 -y install \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+# terra repos
 dnf5 -y install --nogpgcheck --repofrompath \
   'terra,https://repos.fyralabs.com/terra$releasever' terra-release{,-extras,-mesa}
 
-# dnf5 config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+# copr
+dnf5 -y copr enable bieszczaders/kernel-cachyos-lto
+dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
+dnf5 -y copr enable ublue-os/packages
+dnf5 -y copr enable che/nerd-fonts
 
-for copr in "${coprs[@]}"; do
-  echo "Enabling copr: $copr"
-  dnf5 -y copr enable "$copr"
-done
-
-#echo "priority=1" | tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri.repo
-#echo "priority=2" | tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:ulysg:xwayland-satellite.repo
-#echo "priority=3" | tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:avengemedia:danklinux.repo
 dnf5 -y config-manager setopt "*terra*".priority=4 "*terra*".exclude="nerd-fonts topgrade steam python3-protobuf"
 dnf5 -y config-manager setopt "terra-mesa".enabled=true
 dnf5 -y config-manager setopt "*rpmfusion*".priority=5 "*rpmfusion*".exclude="mesa-*"
 dnf5 -y config-manager setopt "*fedora*".exclude="mesa-* kernel-core-* kernel-modules-* kernel-uki-virt-*"
-
-echo "::endgroup::"
