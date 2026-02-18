d5662112a094420fa59028b15c1547ca999945fa
roworu <mamoruessu@gmail.com>
2026-02-18 19:56:56 +0200

    onem more cleanup

--- build-scripts/30-gnome.sh
+++ build-scripts/30-gnome.sh
@@ -3,27 +3,4 @@
 set -ouex pipefail
 
 
-### install packages
-
-packages=(
-    @workstation-product-environment
-)
-
-dnf5 -y install "${packages[@]}"
-
-
-### enable services
-
-preset_file="/usr/lib/systemd/system-preset/01-kyawthuite.preset"
-
-system_services=(
-    gdm
-)
-
-systemctl enable "${system_services[@]}"
-
-for service in "${system_services[@]}"; do
-  echo "enable $service" >> "$preset_file"
-done
-
-systemctl --global preset-all
+# todo: gnomie specific settings
--- build-scripts/30-plasma.sh
+++ build-scripts/30-plasma.sh
@@ -3,35 +3,4 @@
 set -ouex pipefail
 
 
-### install packages
-
-packages=(
-    plasma-desktop
-    plasma-workspace
-    plasma-workspace-x11
-    sddm
-    sddm-breeze
-    sddm-wayland-plasma
-    xorg-x11-server-Xorg
-)
-
-dnf5 -y install "${packages[@]}"
-
-
-### enable services
-
-preset_file="/usr/lib/systemd/system-preset/01-kyawthuite.preset"
-
-system_services=(
-#    plasmalogin
-    sddm.service
-)
-
-systemctl enable "${system_services[@]}"
-systemctl set-default graphical.target
-
-for service in "${system_services[@]}"; do
-  echo "enable $service" >> "$preset_file"
-done
-
-# systemctl --global preset-all
+# todo: plasma specific settings
