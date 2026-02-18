0c1585fcf3e0be4a9b18e0de584025267736a63f
roworu <mamoruessu@gmail.com>
2026-02-18 17:13:14 +0200

    remove more repos, block kmod-nvidia updates

--- build-scripts/00-dnf.sh
+++ build-scripts/00-dnf.sh
@@ -4,18 +4,17 @@ set -ouex pipefail
 
 dnf5 repolist --enabled
 
-
-dnf5 -y install dnf5-plugins
 echo -n "max_parallel_downloads=10" >>/etc/dnf/dnf.conf
 
 # rpmfusion repos
-dnf5 -y install \
-  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+#dnf5 -y install \
+#  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+#  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # copr
 dnf5 -y config-manager setopt "*fedora*".exclude="kernel-core-* kernel-modules-* kernel-uki-virt-*"
 dnf5 -y config-manager setopt "*updates*".exclude="kernel-core-* kernel-modules-* kernel-uki-virt-*"
+dnf5 -y config-manager setopt "*fedora-multimedia*".exclude="akmod-nvidia kmod-nvidia"
 
 # packages install
 dnf5 -y swap ffmpeg-free ffmpeg --allowerasing -y
