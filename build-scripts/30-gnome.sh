108c94e7c4fc5c4dbf5c30ae433f6519f762e991
roworu <mamoruessu@gmail.com>
2026-02-20 15:17:15 +0200

    remove extensions for gnome

--- build-scripts/30-gnome.sh
+++ build-scripts/30-gnome.sh
@@ -2,9 +2,4 @@
 
 set -ouex pipefail
 
-# build extensions
-dnf5 -y install glib2-devel
-glib-compile-schemas /usr/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com/schemas
-
-dnf5 -y remove glib2-devel
-rm -rf /usr/share/gnome-shell/extensions/tmp
+echo foo
