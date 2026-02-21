244485dc5d36d9095565b2fba40a04f393fcf2b7
roworu <mamoruessu@gmail.com>
2026-02-21 14:07:30 +0200

    change sys exec name

--- system_files/base/etc/profile.d/_sys.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-alias sys-update="_sys check"
-alias sys-upgrade="_sys upgrade"
-alias sys-upgrade-reboot="_sys reboot"
---
+++ system_files/base/etc/profile.d/sys.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+alias sys-update="sys check"
+alias sys-upgrade="sys upgrade"
+alias sys-upgrade-reboot="sys reboot"
