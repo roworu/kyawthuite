bd8e2618ad583a37224e0b809a057aa76e1bcf5f
roworu <mamoruessu@gmail.com>
2026-02-21 10:06:44 +0200

    change dnf to have persistant config file

--- build_scripts/00-dnf.sh
+++ build_scripts/00-dnf.sh
@@ -4,13 +4,6 @@ set -ouex pipefail
 
 dnf5 repolist --enabled
 
-echo -n "max_parallel_downloads=10" >>/etc/dnf/dnf.conf
-
-# rpmfusion repos
-#dnf5 -y install \
-#  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-#  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
 # copr
 dnf5 -y config-manager setopt "*fedora*".exclude="kernel-core-* kernel-modules-* kernel-uki-virt-*"
 dnf5 -y config-manager setopt "*updates*".exclude="kernel-core-* kernel-modules-* kernel-uki-virt-*"
@@ -24,7 +17,6 @@ dnf5 -y install zsh fastfetch zram-generator
 
 # virtualization tools (virt manager, etc.)
 dnf5 -y install @virtualization
-# gpasswd -a wheel libvirt
 
 # flatpak setup
 flatpak remote-add --if-not-exists --system flathub /etc/flatpak/remotes.d/flathub.flatpakrepo
