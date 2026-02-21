6b844a4bd24ec397973a1c5909ff6ce437d0f693
roworu <mamoruessu@gmail.com>
2026-02-21 21:48:08 +0200

    change sys shell and format;

--- system_files/base/etc/profile.d/sys.sh
+++ system_files/base/etc/profile.d/sys.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-log() {
+_log() {
     local level="info"
     if [[ "${1:-}" == "error" ]]; then
         level="error"
@@ -15,78 +15,78 @@ log() {
 }
 
 sys-update() {
-    log "==> Checking bootc..."
+    _log "==> Checking bootc..."
     output=$(sudo bootc upgrade --check || true)
 
     if echo "$output" | grep -qi "update available"; then
         version=$(echo "$output" | awk -F': ' '/Version:/ {print $2}')
         if [[ -z "${version:-}" ]]; then
-            log error "Unable to determine bootc update version. Please run: sudo bootc upgrade --check"
+            _log error "Unable to determine bootc update version. Please run: sudo bootc upgrade --check"
             return 1
         fi
-        log "bootc update available: ${version}"
+        _log "bootc update available: ${version}"
     else
-        log "bootc: up to date"
+        _log "bootc: up to date"
     fi
 
-    log "==> Checking flatpak..."
+    _log "==> Checking flatpak..."
     flatpak_output=$(flatpak remote-ls --updates 2>&1)
     flatpak_status=$?
     if [[ $flatpak_status -ne 0 ]]; then
-        log error "Unable to check flatpak updates. Please run: flatpak remote-ls --updates"
+        _log error "Unable to check flatpak updates. Please run: flatpak remote-ls --updates"
         return 1
     fi
     flatpak_updates=$(printf '%s\n' "$flatpak_output" | wc -l | tr -d ' ')
     if [[ ! "$flatpak_updates" =~ ^[0-9]+$ ]]; then
-        log error "Unable to count flatpak updates. Please run: flatpak remote-ls --updates"
+        _log error "Unable to count flatpak updates. Please run: flatpak remote-ls --updates"
         return 1
     fi
     if [[ "$flatpak_updates" -eq 0 ]]; then
-        log "flatpak: no updates"
+        _log "flatpak: no updates"
     else
-        log "flatpak updates: $flatpak_updates"
+        _log "flatpak updates: $flatpak_updates"
     fi
 
-    log "==> Checking brew..."
+    _log "==> Checking brew..."
     if ! brew update >/dev/null 2>&1; then
-        log error "Unable to update brew metadata. Please run: brew update"
+        _log error "Unable to update brew metadata. Please run: brew update"
         return 1
     fi
     brew_output=$(brew outdated 2>&1)
     brew_status=$?
     if [[ $brew_status -ne 0 ]]; then
-        log error "Unable to count brew updates. Please run: brew outdated"
+        _log error "Unable to count brew updates. Please run: brew outdated"
         return 1
     fi
     brew_updates=$(printf '%s\n' "$brew_output" | wc -l | tr -d ' ')
     if [[ ! "$brew_updates" =~ ^[0-9]+$ ]]; then
-        log error "Unable to count brew updates. Please run: brew outdated"
+        _log error "Unable to count brew updates. Please run: brew outdated"
         return 1
     fi
     if [[ "$brew_updates" -eq 0 ]]; then
-        log "brew: no updates"
+        _log "brew: no updates"
     else
-        log "brew updates: $brew_updates"
+        _log "brew updates: $brew_updates"
     fi
 }
 
 sys-upgrade() {
 
-    log "==> Upgrading bootc..."
+    _log "==> Upgrading bootc..."
     sudo bootc upgrade
 
-    log "==> Upgrading flatpak..."
+    _log "==> Upgrading flatpak..."
     flatpak update -y
 
-    log "==> Upgrading brew..."
+    _log "==> Upgrading brew..."
     brew upgrade --refresh
 
-    log "==> Upgrade complete!"
+    _log "==> Upgrade complete!"
 }
 
 sys-upgrade-reboot() {
     upgrade
-    log "==> Rebooting..."
+    _log "==> Rebooting..."
     sudo reboot
 }
 
