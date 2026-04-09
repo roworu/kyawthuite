1b40888eabcb2571f1374b0a6aba216310e63842
roworu <mamoruessu@gmail.com>
2026-04-10 00:06:22 +0300

    + fix silly typo

--- system_files/base/etc/profile.d/fastfetch.sh
+++ system_files/base/etc/profile.d/fastfetch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-alias neofetch="/usr/bin/fastfetch --logo /usr/share/icons/logo.txt --config /usr/share/ublue-os/bazzite/fastfetch.jsonc"
+alias neofetch="/usr/bin/fastfetch --logo /usr/share/icons/logo.txt --config /usr/share/ublue-os/kyawthuite/fastfetch.jsonc"
 
-alias fastfetch="/usr/bin/fastfetch --logo /usr/share/icons/logo.txt --config /usr/share/ublue-os/bazzite/fastfetch.jsonc"
+alias fastfetch="/usr/bin/fastfetch --logo /usr/share/icons/logo.txt --config /usr/share/ublue-os/kyawthuite/fastfetch.jsonc"
