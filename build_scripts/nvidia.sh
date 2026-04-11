de8a345b1d8fe601cfa114c19641b2f50ae3ad90
roworu <mamoruessu@gmail.com>
2026-04-11 17:26:13 +0300

    Create nvidia.sh

---
+++ build_scripts/nvidia.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -ouex pipefail
+shopt -s nullglob
+
+###
+###  dnf
+###
+
+FEDORA_VERSION="$(rpm -E %fedora)"
+dnf5 -y copr enable bieszczaders/kernel-cachyos-lto "fedora-${FEDORA_VERSION}-x86_64"
+dnf5 -y copr enable bieszczaders/kernel-cachyos-addons "fedora-${FEDORA_VERSION}-x86_64"
+
+dnf5 -y config-manager setopt "*fedora*".exclude="kernel-core-* kernel-modules-* kernel-uki-virt-*"
+dnf5 -y config-manager setopt "*updates*".exclude="kernel-core-* kernel-modules-* kernel-uki-virt-*"
+
+dnf5 -y install zsh git
+
+###
+### kernel install
+###
+
+pushd /usr/lib/kernel/install.d
+printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
+printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
+chmod +x  05-rpmostree.install 50-dracut.install
+popd
+
+for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
+  dnf5 -y remove $pkg
+done
+rm -rf /usr/lib/modules/*
+rm -rf /boot/*
+
+# install and lock cachy kernel
+packages=(
+  kernel-cachyos-lto
+  kernel-cachyos-lto-core
+  kernel-cachyos-lto-devel-matched
+  kernel-cachyos-lto-modules
+)
+dnf5 -y install "${packages[@]}"
+dnf5 versionlock add "${packages[@]}"
+
+KERNEL_VERSION=$(ls /usr/lib/modules | head -n1) \
+akmods --force --kernels "${KERNEL_VERSION}" --kmod "nvidia"
